25aa31d0e6109ed0728cf7e4a57e5e60410191b0
YsisC <ysisclongart@gmail.com>
2023-01-24 17:30:33 -0300

    Css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,16 @@
 import { functionSignOut } from '../lib/index.js';
 
 export const Login = (onNavigate) => {
+  const homeDivFeed = document.createElement('div');
+  const feedHearder = document.createElement('header');
   const feedeMain = document.createElement('main');
   const logoIcon = document.createElement('img');
   const mensajeFeed = document.createElement('h2');
   const feedPost = document.createElement('div');
-  const feedHearder = document.createElement('div');
   const feedFooter = document.createElement('div');
   const buttonHome = document.createElement('button');
 
+  homeDivFeed.className = 'homeDivFeed';
   feedeMain.className = 'feedMain';
   logoIcon.src = '/assets/img/LogotipoSinFondo.png';
   logoIcon.className = 'logoFoodgramFeed';
@@ -16,15 +18,16 @@ export const Login = (onNavigate) => {
   feedPost.className = 'feedPost';
   feedFooter.className = 'feedFooter';
   mensajeFeed.textContent = 'Bienvenida al login';
-  buttonHome.className = 'Cerrar Sesion';
+  buttonHome.className = 'Cerrar_Sesion';
   buttonHome.textContent = 'Cerrar Sesion';
   buttonHome.addEventListener('click', () => {
     functionSignOut();
     onNavigate('/');
   });
 
-  feedHearder.appendChild(logoIcon, buttonHome, mensajeFeed);
-  feedeMain.appendChild(feedHearder, feedPost, feedFooter);
+  feedHearder.append(logoIcon, buttonHome, mensajeFeed);
+  feedeMain.append(feedPost);
+  homeDivFeed.append(feedHearder, feedeMain, feedFooter);
 
-  return feedeMain;
+  return homeDivFeed;
 };
