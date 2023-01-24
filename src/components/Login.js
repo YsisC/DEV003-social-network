4195a71c5f6b20762a069d07a328fb5cb13a3ab1
“Nicole <nicolecondeduque@gmail.com>
2023-01-24 14:32:31 -0500

    modificaciones en ccs

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,30 @@
 import { functionSignOut } from '../lib/index.js';
 
 export const Login = (onNavigate) => {
-  const HomeDiv = document.createElement('div');
+  const feedeMain = document.createElement('main');
+  const logoIcon = document.createElement('img');
+  const mensajeFeed = document.createElement('h2');
+  const feedPost = document.createElement('div');
+  const feedHearder = document.createElement('div');
+  const feedFooter = document.createElement('div');
   const buttonHome = document.createElement('button');
 
-  HomeDiv.textContent = 'Bienvenida al login';
+  feedeMain.className = 'feedMain';
+  logoIcon.src = '/assets/img/LogotipoSinFondo.png';
+  logoIcon.className = 'logoFoodgramFeed';
+  feedHearder.className = 'feedProfile';
+  feedPost.className = 'feedPost';
+  feedFooter.className = 'feedFooter';
+  mensajeFeed.textContent = 'Bienvenida al login';
+  buttonHome.className = 'Cerrar Sesion';
   buttonHome.textContent = 'Cerrar Sesion';
   buttonHome.addEventListener('click', () => {
     functionSignOut();
     onNavigate('/');
   });
 
-  HomeDiv.appendChild(buttonHome);
+  feedHearder.appendChild(logoIcon, buttonHome, mensajeFeed);
+  feedeMain.appendChild(feedHearder, feedPost, feedFooter);
 
-  return HomeDiv;
+  return feedeMain;
 };
