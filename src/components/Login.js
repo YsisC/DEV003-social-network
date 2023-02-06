57984494097ae34d5d42040c8c2d6b07d220f979
YsisC <ysisclongart@gmail.com>
2023-02-06 14:32:02 -0300

    lo

--- src/components/Login.js
+++ src/components/Login.js
@@ -66,6 +66,7 @@ export const Login = (onNavigate) => {
   divIconPublish.className = 'divIconPublish';
   iconUser.className = 'fa-solid fa-user';
   iconPublish.className = 'fa-regular fa-square-plus';
+  const usuarioInfo = currentUserInfo();
   const usuario = currentUserInfo().displayName;
   const usuarioId = currentUserInfo().uid;
   mensajeFeed.placeholder = `¿Que recetas estas pensando ${usuario}?`;
@@ -137,14 +138,29 @@ export const Login = (onNavigate) => {
           <h3>${task.tittle}</h3>
           <p>${task.description}<p>
           <p class='displayName'> 👨🏽‍🍳${task.displayName}</p>
-          <div class='btnLikeDiv'>
-          <button class='btn-like' data-id='${doc.id}'>🖤</button>
-          <p class='numLike' data-id='${doc.id}'>${task.like}</p>
-          </div>
+          <div class='iconos'>
+        <button class='buttonLike' data-id = ${doc.id}>
+        <span class='icon'><i class="fa-regular fa-heart like ${task.like.includes(usuarioInfo.email) ? 'true' : 'false'}"></i>
+        </span>
+        <span class='count'>${task.like.length}</span>
+        </button>   </div>
           <button class='btn-delete' data-id='${doc.id}'>Delete</button>
           <button class='btn-edit' data-id='${doc.id}'>Edit</button>
         </div>
           `;
+      // html += `
+      //   <div class='cardPostPublication'>
+      //     <h3>${task.tittle}</h3>
+      //     <p>${task.description}<p>
+      //     <p class='displayName'> 👨🏽‍🍳${task.displayName}</p>
+      //     <div class='btnLikeDiv'>
+      //     <button class='btn-like' data-id='${doc.id}'>🖤</button>
+      //     <p class='numLike' data-id='${doc.id}'>${task.like}</p>
+      //     </div>
+      //     <button class='btn-delete' data-id='${doc.id}'>Delete</button>
+      //     <button class='btn-edit' data-id='${doc.id}'>Edit</button>
+      //   </div>
+      //     `;
     });
 
     taskContainer.innerHTML = html;
